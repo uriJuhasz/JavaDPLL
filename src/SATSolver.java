5e1722a896fc0ea50b2e0178e5c11a6b2729aed9
uri.juhasz@gmail.com <uri.juhasz@gmail.com>
2017-03-07 01:44:13 +0100

    Middle end

--- src/SATSolver.java
+++ src/SATSolver.java
@@ -2,5 +2,16 @@
  * Created by uri on 06/03/2017.
  */
 public class SATSolver {
+    public SATSolver(){}
+
+    public void addCNF(CNF cnf)
+    {
+        for (c : cnf.clauses)
+            addClause(c);
+    }
+    public void addClause(CNF.Clause)
+    {
+
+    }
 
 }
